0ec3414ae7b585f8eba5dd7b33fbae040f093b1c
dddd0ng <kdg1644@naver.com>
2025-11-08 02:39:44 +0900

    [setup] plan 상세보기, 수정, 리뷰작성 DTO 세팅

--- routy-be/src/main/java/com/c4/routy/domain/plan/controller/PlanController.java
+++ routy-be/src/main/java/com/c4/routy/domain/plan/controller/PlanController.java
@@ -1,9 +1,11 @@
 package com.c4.routy.domain.plan.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/mypage")
 public class PlanController {
 }
